bbcfad9023ce8911ea8a15dcc91856aed7ae83bb
Sophiat06 <fulanisurfeeyatonaopemipo@gmail.com>
2023-04-20 11:31:49 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 /**
- * main: Entry point
- * printf: prints output
- *
- * Return: Always 0 (success)
+ * main - Entry point
+ * return - Always 0 (success)
  */
 int main(void)
 {
