22898b413b4bf30091e1832bb3c113de1d9927fb
Sophiat06 <fulanisurfeeyatonaopemipo@gmail.com>
2023-04-20 10:51:19 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
- *
- * main: A C program that prints a line using printf function
+ * main: A c program that prints a line using printf function
  *
  * Return: Always 0 (success)
  */
