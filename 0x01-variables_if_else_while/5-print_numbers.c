8c50c2087088a0625173d15ed861a63dec50f7c5
Sophiat06 <fulanisurfeeyatonaopemipo@gmail.com>
2023-04-21 12:47:27 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char q;
+
+	for (q = '0'; q <= '9'; q++)
+	{
+		putchar(q);
+	}
+	putchar('\n');
+	return (0);
+}
