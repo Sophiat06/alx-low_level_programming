a5c234e0deebf7464327e7061a3811f129918e50
Sophiat06 <fulanisurfeeyatonaopemipo@gmail.com>
2023-04-21 16:29:34 -0700

    102 print comb

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 99; a++)
+	{
+		for (b = b + 1; b <= 99; b++)
+		{
+			putchar('0' + a / 10);
+			putchar('0' + a % 10);
+
+			putchar(' ');
+			putchar('0' + b / 10);
+			putchar('0' + b % 10);
+
+			if (a == 98 && b == 99)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
