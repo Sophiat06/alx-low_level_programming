ce284edb65433c88e81230e42b3fd62f196cc6c5
Sophiat06 <fulanisurfeeyatonaopemipo@gmail.com>
2023-04-21 11:50:27 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print letters in lower case
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+
+	{
+		putchar(l)
+	}
+
+	putchar('\n');
+
+	return (0);
+}
