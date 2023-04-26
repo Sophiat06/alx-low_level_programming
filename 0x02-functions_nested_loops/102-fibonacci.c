4c7044854ae92373e2ef2acb83b8f9a6c47b89e2
Sophiat06 <fulanisurfeeyatonaopemipo@gmail.com>
2023-04-26 07:02:56 -0700

    fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,25 +6,24 @@
  */
 int main(void)
 {
-	unsigned long int sum3, sum5, sum;
-	int i;
+	long int i, j, k, next;
 
-	sum3 = 0;
-	sum5 = 0;
-	sum = 0;
+	j = 1;
+	k = 2;
 
-	for (i = 0; i < 1024; ++i)
+	for (i = 1; i <= 50; ++i)
 	{
-		if ((i % 3) == 0)
+		if (j != 20365011074)
 		{
-			sum3 = sum3 + i;
+			printf("%ld, ", j);
 		}
-		else if ((i % 5) == 0)
+		else
 		{
-			sum5 = sum5 + i;
+			printf("%ld\n", j);
 		}
+		next = j + k;
+		j = k;
+		k = next;
 	}
-	sum = sum3 + sum5;
-	printf("%lu\n", sum);
 	return (0);
 }
