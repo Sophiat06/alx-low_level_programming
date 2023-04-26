dee25abe90ed11edf5908d5e2cba7c582696502d
Sophiat06 <fulanisurfeeyatonaopemipo@gmail.com>
2023-04-26 01:50:15 -0700

    100 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,7 +6,6 @@
 void print_times_table(int n)
 {
 	int a, b, c;
-
 	if (n >= 0 && n <= 15)
 	{
 		for (a = 0; a <= n; a++)
@@ -30,9 +29,11 @@ void print_times_table(int n)
 					_putchar((c % 10) + 48);
 				}
 				else
+				{
 					_putchar(((c / 100) % 10) + 48);
 					_putchar(((c / 10) % 10) + 48);
 					_putchar((c % 10) + 48);
+				}
 			}
 			_putchar('\n');
 		}
