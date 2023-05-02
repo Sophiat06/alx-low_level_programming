de6ab8431e8b157832b3ae282f578cdf6f181ceb
Sophiat06 <fulanisurfeeyatonaopemipo@gmail.com>
2023-05-02 08:17:21 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - finds the length of a string
+ * @s: string pointer to the string whose length is to be found
+ * Return: return the length of the string
+ */
+int _strlen(char *s)
+{
+	int p = 0;
+
+	while (*(s + p) != 0)
+	{
+		p++;
+	}
+	return (p);
+}
