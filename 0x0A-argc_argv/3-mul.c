4c2fb7a3686428490e0047d263f621c1c60063f0
Sophiat06 <fulanisurfeeyatonaopemipo@gmail.com>
2023-05-12 15:13:03 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -40,4 +40,28 @@ int _atoi(char *s)
 	if (f == 0)
 		return (0);
 	return (n);
+
+}
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (success), 1 (error)
+ */
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+	return (0);
 }
