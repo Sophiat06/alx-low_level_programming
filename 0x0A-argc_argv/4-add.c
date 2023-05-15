33f937e95149dd826b8a1f63e700c31b548a8354
Sophiat06 <fulanisurfeeyatonaopemipo@gmail.com>
2023-05-15 07:06:24 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -43,7 +43,7 @@ int main(int argc, char *argv[])
 	{
 		if (check_num(argv[count]))
 		{
-			str_to_int = _atoi(argv[count]);
+			str_to_int = atoi(argv[count]);
 			sum += str_to_int;
 		}
 		else
