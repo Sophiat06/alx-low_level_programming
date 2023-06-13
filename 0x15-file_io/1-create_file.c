ce015503bba13698c133063583611139b02eaf12
Sophiat06 <fulanisurfeeyatonaopemipo@gmail.com>
2023-06-13 09:47:57 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * create_file - function that creates a file
+ * @filename: a pointer to the file to be created
+ * @text_content: a pointer to the string which is to be written in the file
+ *
+ * Return: 1 if success and -1 if failure
+ * or if filename is NULL
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int a, c, length;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (length = 0; text_content[length];)
+			length++;
+	}
+	a = open(filename, O_CREAT | O_RDWR || O_TRUNC, 0600);
+	c = write(a, text_content, length);
+
+	if (a == -1 || c == -1)
+		return (-1);
+	close(a);
+	return (1);
+}
