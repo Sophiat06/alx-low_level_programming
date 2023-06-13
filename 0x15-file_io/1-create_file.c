785d67263615fb764ef343aac3154b4089a19957
Sophiat06 <fulanisurfeeyatonaopemipo@gmail.com>
2023-06-13 10:20:55 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,21 +8,24 @@
  * Return: 1 if success and -1 if failure
  */
 int create_file(const char *filename, char *text_content)
-{
-	int a, c, length;
+{int a, c, length = 0;
 
 	if (filename == NULL)
 		return (-1);
+
 	if (text_content != NULL)
 	{
 		for (length = 0; text_content[length];)
 			length++;
 	}
-	a = open(filename, O_CREAT | O_RDWR || O_TRUNC, 0600);
+
+	a = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	c = write(a, text_content, length);
 
 	if (a == -1 || c == -1)
 		return (-1);
+
 	close(a);
+
 	return (1);
 }
